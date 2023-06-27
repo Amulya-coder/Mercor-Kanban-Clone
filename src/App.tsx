bb75c4aa3c3742bb5626c999691fef80109ab795
Amulya Dixit <amulyadixit07@gmail.com>
2023-06-27 13:02:04 +0530

    Complete

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <div className="h-screen w-full flex">
+      <Sidebar className="hidden md:block" />
+      <main className="flex-1">
+        <Navbar />
+        <Main />
+      </main>
+    </div>
+  );
+};
+
+export default App;
